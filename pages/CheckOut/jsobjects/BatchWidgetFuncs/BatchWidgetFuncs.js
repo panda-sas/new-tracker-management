899fb6103ad88a3bdd536edbde09f63a1c8a3cd3
Saswati Panda <panda@tamu.edu>
2023-02-16 03:27:53 +0000

    Beautification

--- pages/CheckOut/jsobjects/BatchWidgetFuncs/BatchWidgetFuncs.js
+++ pages/CheckOut/jsobjects/BatchWidgetFuncs/BatchWidgetFuncs.js
@@ -1,17 +1,17 @@
 export default {
 
-	updateBatchDetailsContainer: () => {
-
-		FETCH_BATCH_DETAILS.run().then((rows) => {
-			TargetInputText.text = rows[0]['Target']
-			LocationTextValue.text = rows[0]['Location']
-			ProjectTextValue.text = rows[0]['Project']
-
-			FETCH_ORDERS.run({orderId : rows[0]['OrderId']}).then((rows => {
-				DispatcherTextValue.text = rows[0]['Dispatcher']
-			}))
-
-		})
-
+	updateBatchDetailsContainer: async () => {
+		
+		await FETCH_BATCH_DETAILS.run();
+		let batchData = FETCH_BATCH_DETAILS.data[0]
+		
+		await FETCH_ORDERS.run({orderId : batchData['OrderId']})
+		let orderData = FETCH_ORDERS.data[0]
+		
+		/* Update labels*/
+		TargetInputText.text = batchData['Target']
+		LocationTextValue.text = batchData['Location']
+		ProjectTextValue.text = batchData['Project']
+		DispatcherTextValue.text = orderData['Dispatcher']
 	}
 }
