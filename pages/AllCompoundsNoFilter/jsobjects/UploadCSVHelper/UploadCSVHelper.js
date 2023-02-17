a2b9f6f11e16a6a2232d9bdbf6172f4660ad625e
Sid Rath <sid@tamu.edu>
2023-02-17 17:15:34 +0000

    csv upload

--- pages/AllCompoundsNoFilter/jsobjects/UploadCSVHelper/UploadCSVHelper.js
+++ pages/AllCompoundsNoFilter/jsobjects/UploadCSVHelper/UploadCSVHelper.js
@@ -1,24 +1,86 @@
 export default {
-	csvJSON: (csv) => {
+ 
+  csv2JSON: (strData, strDelimiter) => {
+    // then default to comma.
+    strDelimiter = strDelimiter || ",";
 
-  var lines=csv.split("\n");
+    // Create a regular expression to parse the CSV values.
+    var objPattern = new RegExp(
+      // Delimiters.
+      "(\\" +
+        strDelimiter +
+        "|\\r?\\n|\\r|^)" +
+        // Quoted fields.
+        '(?:"([^"]*(?:""[^"]*)*)"|' +
+        // Standard fields.
+        '([^"\\' +
+        strDelimiter +
+        "\\r\\n]*))",
+      "gi"
+    );
 
-  var result = [];
+    // Create an array to hold our data. Give the array
+    // a default empty first row.
+    var arrData = [[]];
 
-  var headers=lines[0].split(",");
+    // Create an array to hold our individual pattern
+    // matching groups.
+    var arrMatches = null;
 
-  for(let i=1;i<lines.length;i++){
+    // Keep looping over the regular expression matches
+    // until we can no longer find a match.
+    while ((arrMatches = objPattern.exec(strData))) {
+      // Get the delimiter that was found.
+      var strMatchedDelimiter = arrMatches[1];
 
-	  var obj = {};
-	  var currentline=lines[i].split(",");
+      // Check to see if the given delimiter has a length
+      // (is not the start of string) and if it matches
+      // field delimiter. If id does not, then we know
+      // that this delimiter is a row delimiter.
+      if (strMatchedDelimiter.length && strMatchedDelimiter !== strDelimiter) {
+        // Since we have reached a new row of data,
+        // add an empty row to our data array.
+        arrData.push([]);
+      }
 
-	  for(var j=0;j<headers.length;j++){
-		  obj[headers[j]] = currentline[j];
-	  }
+      var strMatchedValue;
 
-	  result.push(obj);
+      // Now that we have our delimiter out of the way,
+      // let's check to see which kind of value we
+      // captured (quoted or unquoted).
+      if (arrMatches[2]) {
+        // We found a quoted value. When we capture
+        // this value, unescape any double quotes.
+        strMatchedValue = arrMatches[2].replace(new RegExp('""', "g"), '"');
+      } else {
+        // We found a non-quoted value.
+        strMatchedValue = arrMatches[3];
+      }
 
-  }
-  return result;
-}
-}
+      // Now that we have our value string, let's add
+      // it to the data array.
+      arrData[arrData.length - 1].push(strMatchedValue);
+    }
+
+    var headers = arrData[0];
+    var result = [];
+
+    for (let i = 1; i < arrData.length; i++) {
+      var obj = {};
+      var currentline = arrData[i];
+
+      for (var j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentline[j];
+      }
+			
+			// Insert Compound ID and Batch ID
+			obj['CompoundId'] = Generator.getUUID();
+			obj['BatchId'] = appsmith.URL.queryParams.BatchId
+
+      result.push(obj);
+    }
+
+    // Return the parsed data.
+    return result;
+  },
+};
